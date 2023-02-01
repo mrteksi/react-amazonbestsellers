8ad0fa59d022235b576e828ee4a3f724369e845e
mrteksi <96991589+mrteksi@users.noreply.github.com>
2023-02-01 16:53:25 +0300

    Add files via upload

---
+++ books.js
@@ -0,0 +1,18 @@
+import img1 from "./images/1.jpg"
+import img2 from "./images/2.jpg"
+
+export const books = [
+    {
+      author: "Prince Harry",
+      title: "Spare Hardcover",
+      img: img2,
+      id: 1,
+    },
+    {
+      author: "James Clear",
+      title: "Atomic Habits",
+      img: img1,
+      id: 2,
+    },
+  ];
+
